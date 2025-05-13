2605746625a4b158f3c76d93a094a6137640bb53
Peter Doak (epd) <doakpw@ornl.gov>
2025-05-13 19:10:20 -0400

    redo batch skest integration with EMN

--- src/Estimators/EstimatorManagerInput.cpp
+++ src/Estimators/EstimatorManagerInput.cpp
@@ -91,6 +91,8 @@ void EstimatorManagerInput::readXML(xmlNodePtr cur)
         appendEstimatorInput<MagnetizationDensityInput>(child);
       else if (atype == "energydensity")
         appendEstimatorInput<EnergyDensityInput>(child);
+      else if (atype == "structurefactor")
+	appendEstimatorInput<StructureFactorInput>(child);
       else
         throw UniformCommunicateError(error_tag + "unparsable <estimator> node, name: " + aname + " type: " + atype +
                                       " in Estimators input.");
