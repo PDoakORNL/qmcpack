64bf9712bdd6d09c60873da1540cfbb39c0b4804
Peter Doak (epd) <doakpw@ornl.gov>
2025-05-14 11:57:06 -0400

    default names for source and target particle sets

--- src/Estimators/StructureFactorInput.h
+++ src/Estimators/StructureFactorInput.h
@@ -55,8 +55,8 @@ private:
 
   std::string name_{"sk"};
   std::string type_{"sk"};
-  std::string source_;
-  std::string target_;
+  std::string source_{"ion0"};
+  std::string target_{"e"};
   bool write_hdf5_{false};
   bool write_rho_{false};
   bool write_ion_ion_{false};
